b00d4ae6afe867760acbe0f2a7b44160b4a92dc7
Asghar Nazir <asghar@syntaxtech.com>
2022-06-18 15:57:47 -0400

    Class 19 Inheritance.

--- src/com/syntax/class19/Dog.java
+++ src/com/syntax/class19/Dog.java
@@ -12,6 +12,7 @@ class Animal{
 }
 public class Dog extends Animal{
     Dog(String name,String color){
+
         this.name=name; // using extends keyword we
         // are able to access the name field from the animal class
         this.color=color;
