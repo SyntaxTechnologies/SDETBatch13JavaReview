00352f19d676530683cf98cca75fbee8bd4fd1fe
Asghar Nazir <asghar@syntaxtech.com>
2022-06-22 22:02:50 -0400

    Class 22 Overloading.

--- src/com/syntax/class22/AdvanceCalc.java
+++ src/com/syntax/class22/AdvanceCalc.java
@@ -22,14 +22,31 @@ public class AdvanceCalc {
        printNames("Aladin","Aladin","Aladin");
     }
 
+    // Var args demo
     static void printNames(String ... names){
         for (String name:names){
             System.out.println(name);
+
         }
     }
 
     public static void main(String[] args) {
-        sum(2,2,3,4,10);
+      //  sum(2,2,3,4,10);
         //printNames("Aladin","Aladin","Aladin");
+        int age=10;
+        String name;
+        if(age>18){
+            name="Time to go work";
+        }else{
+            name="Enjoy no Work";
+        }
+        System.out.println(name);
+
+        //Ternary operator
+        String name2=age>18? "Time to go work ":"Enjoy no Work";
+      //  String name2=age>18? age>10?"nested in":" it is nested else":"Enjoy no Work";
+        System.out.println(name2);
+
+
     }
 }
