c1bfef003da98566c48e034eea95aae40d514797
Syntax <asgharkrk@gmail.com>
2022-06-23 20:42:19 -0400

    Class 22 Overloading.

--- src/com/syntax/interview/CountDuplicate2.java
+++ src/com/syntax/interview/CountDuplicate2.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class CountDuplicate2 {
     public static void main(String[] args) {
         int [] arr={20,20,40,50,30,30,30,30};
-        countDuplicates(arr); //test
+        countDuplicates(arr);
     }
 
     static void countDuplicates(int [] arr){
