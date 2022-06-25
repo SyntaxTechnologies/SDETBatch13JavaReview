5db3e436370d808d7683fe3ad8879e39e868ab36
Syntax <asgharkrk@gmail.com>
2022-06-25 11:04:01 -0400

    Class 23 PolyMorphism.

---
+++ src/com/syntax/class23/PolyMorphismDemo1.java
@@ -0,0 +1,12 @@
+package com.syntax.class23;
+
+public class PolyMorphismDemo1 {
+
+    void doSomething(String name){
+        System.out.println("downloading a movie from the internet "+name);
+    }
+
+    void doSomething(int num){
+        System.out.println("calculating the text based on new rules");
+    }
+}
