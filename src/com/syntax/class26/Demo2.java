d5899b993b0c7f11f66f1e5e12a20228b48a0edd
Syntax <asgharkrk@gmail.com>
2022-07-03 15:33:54 -0400

    Collections framework

--- src/com/syntax/class26/Demo2.java
+++ src/com/syntax/class26/Demo2.java
@@ -6,14 +6,16 @@ public class Demo2 {
     public static void main(String[] args) {
         ArrayList<Character> arrayList=new ArrayList<>();
         arrayList.add('A');
-        arrayList.add('B');
+        arrayList.add('B'); // adds the element to an arraylist
         arrayList.add('C');
         arrayList.add('D');
         System.out.println(arrayList);
-        System.out.println(arrayList.get(2));
+        System.out.println(arrayList.get(2)); // gets back the elements from the list
 
-        for(int i=0; i<arrayList.size();i++){
+        for(int i=0; i<arrayList.size();i++)
+        {
             System.out.println(arrayList.get(i));
         }
+
     }
 }
