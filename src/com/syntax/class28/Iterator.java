b00fe831e0b496bd9b453b375dd2442bf8593760
Syntax <asgharkrk@gmail.com>
2022-07-16 10:57:08 -0400

    Class 28

--- src/com/syntax/class28/Iterator.java
+++ src/com/syntax/class28/Iterator.java
@@ -1,7 +1,17 @@
 package com.syntax.class28;
 
+import java.util.ArrayList;
+
 public class Iterator {
     public static void main(String[] args) {
+
+
+        ArrayList<String> words=new ArrayList<>();
+        words.add("Hope");
+        words.add("Play");
+        words.add("Come");
+        words.add("Frame");
+        words.add("game");
 
     }
 }
